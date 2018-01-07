74233c8bfde90ca6af86dd6c09158c1b424007ca
Eric Marrone <emarrone17@gmail.com>
2018-01-06 22:41:34 -0500

    Add javascript to prepend new tweet. Fix submit button from being disabled after new tweet

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,19 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   var form = document.querySelector('#new_tweet');
 
-  form.addEventListener('submit', function(e){
-      e.preventDefault();
-      $.ajax({
-        url:  form.getAttribute('action'),
-        method: form.getAttribute('method'),
-        dataType:  'HTML',
-        data: $(form).serialize()
-      }).done(function(data){
-        console.log(data);
-      });
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: form.getAttribute('action'),
+      method: form.getAttribute('method'),
+      dataType: 'HTML',
+      data: $(form).serialize()
+    }).done(function(data) {
+      console.log(data);
+      //Reset text field after submitting
+      var inputField = document.querySelector("#tweet_message")
+      inputField.value = "";
 
+      //Prepend tweet to list
+      var tweetList = document.querySelector('.tweets');
+      tweetList.insertAdjacentHTML('afterbegin', data)
+    });
   });
-
 });
